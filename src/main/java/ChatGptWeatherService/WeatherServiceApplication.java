082d58021713f23dc5288e5af759ecb535c4f5c2
Hans Reier Sigmond <reier.sigmond@gmail.com>
2023-05-04 20:09:30 +0200

    fikset opp feil.

---
+++ src/main/java/ChatGptWeatherService/WeatherServiceApplication.java
@@ -0,0 +1,27 @@
+package ChatGptWeatherService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class WeatherServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    public void getWeather() {
+        String url = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.911491&lon=10.757933";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+        System.out.println(result);
+    }
+}
---
+++ src/test/java/ChatGptWeatherService/WeatherServiceTest.java
@@ -0,0 +1,44 @@
+package ChatGptWeatherService;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WeatherServiceTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void testGetWeather() {
+        String apiKey="33";
+        String cityName = "Oslo";
+        String url = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                url, HttpMethod.GET, entity, String.class, cityName, apiKey);
+
+        assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+        assertThat(response.getBody()).isNotNull();
+    }
+}
